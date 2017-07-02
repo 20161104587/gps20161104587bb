b51430263eadc4fb42a8e300c20ccbf8491cf04a
student40 <student40@admindeimac-2.local>
2017-07-03 07:31:53 +0800

    Initial Commit

---
+++ gps20161104587bb/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  gps20161104587bb
+//
+//  Created by student40 on 17/7/3.
+//  Copyright © 2017年 student40. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
